d8f2d88f95a0025092af06f52b2dc761e09014fc
Sulyvan Debraize <sulyvan.debraize@net-c.com>
2018-12-18 16:18:05 +0100

    Exercice completé

---
+++ Boucles-Exercice-6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boucles_Exercice_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] months = new string[] { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre" };
+            foreach (string month in months)
+            {
+                Console.WriteLine(month);
+            }
+        }
+    }
+}
